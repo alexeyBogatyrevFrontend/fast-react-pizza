b4e751ef5255e3539a5682ea2de8458e06b3e51d
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-12-26 07:57:31 +0600

    add route error

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,15 +1,24 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom'
 
-function NotFound() {
-  const navigate = useNavigate();
-
-  return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
-    </div>
-  );
+type RouteError = {
+	data?: string
+	message: string
+	stack: string
+	status: number
+	statusText: string
 }
 
-export default NotFound;
+function Error() {
+	const navigate = useNavigate()
+	const error: RouteError = useRouteError() as RouteError
+
+	return (
+		<div>
+			<h1>Something went wrong 😢</h1>
+			<p>{error.data || error.message}</p>
+			<button onClick={() => navigate(-1)}>&larr; Go back</button>
+		</div>
+	)
+}
+
+export default Error
